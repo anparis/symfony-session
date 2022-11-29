cc99c7f8610d1de647341b62172d8ab04ae237bc
Antonin Paris <antonin.pariss@gmail.com>
2022-11-29 15:47:28 +0100

    display past/future/current sessions

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Session;
+use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
@@ -16,51 +17,100 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class SessionRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Session::class);
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Session::class);
+  }
+
+  public function save(Session $entity, bool $flush = false): void
+  {
+    $this->getEntityManager()->persist($entity);
+
+    if ($flush) {
+      $this->getEntityManager()->flush();
     }
+  }
 
-    public function save(Session $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
+  public function remove(Session $entity, bool $flush = false): void
+  {
+    $this->getEntityManager()->remove($entity);
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+    if ($flush) {
+      $this->getEntityManager()->flush();
     }
+  }
 
-    public function remove(Session $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
+  
+  // Return sessions that are older than current date
+  public function sessionsPast()
+  {
+    $now = new DateTime();
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
+    $entityManager = $this->getEntityManager();
 
-//    /**
-//     * @return Session[] Returns an array of Session objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    $query = $entityManager->createQuery(
+      'SELECT s
+            FROM App\Entity\Session s
+            WHERE s.date_fin < :date'
+    )->setParameter('date', $now);
 
-//    public function findOneBySomeField($value): ?Session
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    return $query->getResult();
+  }
+
+  // Return sessions that are happening currently
+  public function sessionsCurrent()
+  {
+    $now = new DateTime();
+
+    $entityManager = $this->getEntityManager();
+
+    $query = $entityManager->createQuery(
+      'SELECT s
+      FROM App\Entity\Session s
+      WHERE s.date_debut <= :date AND s.date_fin >= :date'
+    )->setParameter('date', $now);
+
+    return $query->getResult();
+  }
+
+  // Return sessions that will happen
+  public function sessionsFuture()
+  {
+    $now = new DateTime();
+
+    $entityManager = $this->getEntityManager();
+
+    $query = $entityManager->createQuery(
+      'SELECT s
+            FROM App\Entity\Session s
+            WHERE s.date_debut > :date'
+    )->setParameter('date', $now);
+
+    return $query->getResult();
+  }
+
+  //    /**
+  //     * @return Session[] Returns an array of Session objects
+  //     */
+  //    public function findByExampleField($value): array
+  //    {
+  //        return $this->createQueryBuilder('s')
+  //            ->andWhere('s.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->orderBy('s.id', 'ASC')
+  //            ->setMaxResults(10)
+  //            ->getQuery()
+  //            ->getResult()
+  //        ;
+  //    }
+
+  //    public function findOneBySomeField($value): ?Session
+  //    {
+  //        return $this->createQueryBuilder('s')
+  //            ->andWhere('s.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->getQuery()
+  //            ->getOneOrNullResult()
+  //        ;
+  //    }
 }
