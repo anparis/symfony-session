3c7a4a398fc7f515f9c6e6fc38055a389ba7c968
Antonin Paris <antonin.pariss@gmail.com>
2022-12-02 11:31:45 +0100

    fix: display modules by categories, added module list

---
+++ src/Controller/ModuleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Module;
+use App\Entity\Session;
+use App\Form\ModuleType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ModuleController extends AbstractController
+{
+    #[Route('/module', name: 'app_module')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $modules = $doctrine->getRepository(Module::class)->findAll();
+
+        return $this->render('module/index.html.twig',[
+          'modules' => $modules,
+        ]);
+    }
+
+    #[Route('/module/add', name: 'add_module')]
+    public function add(ManagerRegistry $doctrine, Module $module = null, Request $request): Response
+    {
+        
+        $form = $this->createForm(ModuleType::class, $module);
+        $form->handleRequest($request);
+        
+        // Traitement du formulaire
+        // isValid = filterInput
+        if($form->isSubmitted() && $form->isValid()){
+          //objet session hydrate par les donnees du formulaire
+          $module = $form->getData();
+  
+          // Manager de doctrine, permet d'acceder au persist et au flush
+          $entityManager = $doctrine->getManager();
+          // Prepare objet 
+          $entityManager->persist($module);
+          // Execute = Insert Into 
+          $entityManager->flush();
+  
+          return $this->redirectToRoute('app_module');
+        }
+        return $this->render('module/add.html.twig',[
+          'formModule' => $form->createView(),
+          // 'edit' => $session->getId()
+        ]);
+    }
+
+}
