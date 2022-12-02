1e95b3603696596cd0870d01b18019399cf7f739
Antonin Paris <antonin.pariss@gmail.com>
2022-12-02 14:04:25 +0100

    added categorie list with edit/add fonctionnality

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'app_categorie')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+      $categories = $doctrine->getRepository(Categorie::class)->findAll();
+      return $this->render('categorie/index.html.twig',[
+        'categories' => $categories,
+      ]);
+    }
+
+    #[Route('/categorie/add', name: 'add_categorie')]
+    #[Route('/categorie/{id}/edit', name: 'edit_categorie')]
+    public function add(ManagerRegistry $doctrine, Categorie $categorie = null, Request $request): Response
+    {
+        if(!$categorie)
+        {
+          $categorie = new Categorie();
+        }
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+        
+        // Traitement du formulaire
+        // isValid = filterInput
+        if($form->isSubmitted() && $form->isValid()){
+          //objet session hydrate par les donnees du formulaire
+          $categorie = $form->getData();
+  
+          // Manager de doctrine, permet d'acceder au persist et au flush
+          $entityManager = $doctrine->getManager();
+          // Prepare objet 
+          $entityManager->persist($categorie);
+          // Execute = Insert Into 
+          $entityManager->flush();
+  
+          return $this->redirectToRoute('app_categorie');
+        }
+        return $this->render('categorie/add.html.twig',[
+          'formCategorie' => $form->createView(),
+          'edit' => $categorie->getId()
+        ]);
+    }
+}
--- src/Form/ModuleType.php
+++ src/Form/ModuleType.php
@@ -4,11 +4,12 @@ namespace App\Form;
 
 use App\Entity\Module;
 use App\Entity\Categorie;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ModuleType extends AbstractType
 {
@@ -20,12 +21,15 @@ class ModuleType extends AbstractType
       ->add('categorie', EntityType::class, [
         // looks for choices from this entity
         'class' => Categorie::class,
-    
+
         'choice_label' => 'nom',
-    
+
         // 'multiple' => true,
         // 'expanded' => true,
-    ]);
+      ])
+      ->add('submit', SubmitType::class, [
+        'attr' => ['class' => 'btn']
+      ]);
   }
 
   public function configureOptions(OptionsResolver $resolver): void
